dd17e564a96456882287759fa318745db0eab3ec
Dhriti Prasanna Paul <52440734+dhritippaul@users.noreply.github.com>
2020-06-02 01:14:05 +0530

    ARRAYS
    Basics of Array!

---
+++ ARRAYS/two_pointer_sum.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a[] = { 1,3,5,7,10,11,12,13};
+	int s = 16;
+	
+	int i = 0 ;
+	int j = sizeof(a)/sizeof(int) - 1;
+	
+	while(i<j){
+		int current_sum = a[i]+a[j];
+		if (current_sum > s)
+		{
+			j--;
+		}
+		else if(current_sum < s){
+			i++;
+		}
+		else if(current_sum == s)
+		{
+			cout<<a[i]<< " and "<<a[j]<<endl;
+			i++;
+			j--;
+		}
+	}
+	
+	return 0;
+}
