7f38c81d008889f6f1bd1ffc0395de359f6c3ede
Dhriti Prasanna Paul <52440734+dhritippaul@users.noreply.github.com>
2020-08-15 20:43:37 +0530

    Add files via upload

---
+++ ARRAYS/1_Array_intro.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ; 
+    cin>>n; //declaring array elements
+    int a[n];
+    int i ; 
+    //taking inpit in the array
+    for( i = 0;i  < n ; i ++){
+        cin>>a[i];
+    }
+    //output of the array
+    for( i =  0  ; i < n ; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    int s;
+    //size of the array
+    cout<<sizeof(a)<<endl;
+    //no of elements in the array
+     s = sizeof(a)/sizeof(int);
+     cout<<s<<endl;
+    return 0 ;
+}
